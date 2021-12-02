73aac72f2cabaca22d17bd73cfa9eddc1fe5eca2
tanyanu <shah.tany@northeastern.edu>
2021-12-02 16:53:18 -0500

    Set initial theme for the application

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,18 +1,40 @@
 import './App.scss';
 import { Projects } from '../pages/Projects/Projects';
-import { Navbar } from '../components/Navbar/Navbar';
+import Navbar from '../components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
-import SignInOutContainer from "../pages/Login/LoginContainer/LoginContainer";
+import SignInOutContainer from '../pages/Login/LoginContainer/LoginContainer';
 
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1f487e',
+    },
+    secondary: {
+      main: '#89B0AE',
+    },
+  },
+});
+
+theme = createTheme(theme, {
+  palette: {
+    info: {
+      main: theme.palette.secondary.main,
+    },
+  },
+});
 function App() {
   return (
-    <div className='App'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<SignInOutContainer />} />
-        <Route path='projects' element={<Projects />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<SignInOutContainer />} />
+          <Route path='projects' element={<Projects />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
