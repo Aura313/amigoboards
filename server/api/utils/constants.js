77adc55b46ad17d7858453cf2acc712336433c6b
Sravya Ganda <ganda.s@northeastern.edu>
2021-12-05 12:25:11 -0500

    changes related to sign up and login functionality

--- server/api/utils/constants.js
+++ server/api/utils/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-  PORT: 7000,
+  PORT: 4000,
   MONGODB_Connection: "mongodb://localhost:27017/projectmanagement",
   UNIQUE_EMAIL_USER_ERR: "Email or Username already exists",
   JWT_KEY: "boards_Key",
