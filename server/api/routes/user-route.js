bca3635e9b5e68d89f0f678ba4535c4f9545da60
Sravya Ganda <ganda.s@northeastern.edu>
2021-12-05 22:33:14 -0500

    resolved merge conflicts

--- server/api/routes/user-route.js
+++ server/api/routes/user-route.js
@@ -1,22 +1,21 @@
 import express from "express";
 import * as userController from "../controllers/user-controller.js";
-import {tokencheck} from '../services/auth-service.js';
+import { tokencheck } from "../services/auth-service.js";
 const router = express.Router();
 
 /**
  * Router and Controller Binding
  */
- //Route for Registration
- router
-    .route("/signup")
-    .post(userController.validateUser(), userController.createUser);
+//Route for Registration
+router
+  .route("/signup")
+  .post(userController.validateUser(), userController.createUser);
 
-  // Route for logging in the registered user
-  router.route("/login").post(userController.loginUser);
+// Route for logging in the registered user
+router.route("/login").post(userController.loginUser);
 
-router.route("/").get(tokencheck,userController.index);
+router.route("/").get(tokencheck, userController.index);
 
-router.route("/:id").put(tokencheck,userController.update);
-
+router.route("/:id").put(tokencheck, userController.update);
 
 export default router;
