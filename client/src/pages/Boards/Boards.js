ccd0245a3bf7afc0e1c2420322624caa0aa5df25
vyshnavinu <kotha.s@northeastern.edu>
2021-12-06 22:49:56 -0500

    Boards

--- client/src/pages/Boards/Boards.js
+++ client/src/pages/Boards/Boards.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import TodoPaper from "./TodoPaper";
 import DoingPaper from "./DoingPaper";
 import DonePaper from "./DonePaper";
+import axios from '../../middleware/axios';
+import Config from '../../Configuration/Config.json';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,11 +19,32 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+
+
 export default function SimplePaper() {
     const classes = useStyles();
+    const [projects, setProjects] = useState([])
+    const [todos, setTodos] = useState([]);
+
+    // const fetchStatusDetails = async () => {
+//     await axios
+//       .get(`${Config.userStories_url}/${params.id}`)
+//       .then((res) => console.log(res.data));
+//   };
+
+    useEffect(() => {
+        // setOpen(true);
+        const fetchProjects = async () => {
+           await axios.get(Config.projects_url).then(response => setProjects(response.data))
+        };
+        fetchProjects();
+      }, []);
+    
+      console.log(projects, "projects")
 
     return (
         <div className={classes.root}>
+            
             <Paper elevation={3}>
                 <TodoPaper />
             </Paper>
